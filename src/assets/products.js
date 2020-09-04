c3329cff5d574b3407c518108f683539277c3bed
spect1 <emmajnrk@gmail.com>
2020-09-04 11:39:42 +0100

    added more products

--- src/assets/products.js
+++ src/assets/products.js
@@ -95,7 +95,7 @@ const products = [
     ]
   },
   {
-    id: 4,
+    id: 5,
     title: "vica extras",
     imageSrc: require("../assets/cake.png"),
     shortDescription: `fter massive project practice and summaries, 
@@ -112,27 +112,11 @@ const products = [
     benefits: ["V-Ca "]
   },
   {
-    id: 5,
-    title: "vica extras",
-    imageSrc: require("../assets/cake.png"),
-    shortDescription: `fter massive project practice and summaries, 
-    Ant Design, a design language for background applications, 
-    is refined by Ant UED Team, which aims touniform the user`,
-    price: "100",
-    description: `After massive project practice and summaries, 
-      Ant Design, a design language for background applications, 
-      is refined by Ant UED Team, which aims touniform the user
-      interface specs for internal background projects, 
-      lower the unnecessary cost ofdesign differences and 
-      implementation and liberate the resources of 
-      design and front-end development.`
-  },
-  {
     id: 6,
     title: "Cordy Active",
     benefits: [
-      "Effective for Asthma", 
-      "Effective for Cardiovascular Diseases", 
+      "Effective for Asthma",
+      "Effective for Cardiovascular Diseases",
       "Effective for Lack of Appetite",
       "Effective for Lack of Viitalite",
       "Effective for Anti-ageing",
@@ -141,7 +125,7 @@ const products = [
   },
   {
     id: 7,
-    title:"Golden Hypha",
+    title: "Golden Hypha",
     benefits: [
       "Immune system booter",
       "Relieves side effect of treatment of cancer and tumor",
@@ -152,7 +136,7 @@ const products = [
   },
   {
     id: 8,
-    title:"Gastrifort",
+    title: "Gastrifort",
     benefits: [
       "Effective for Stomach and liver diseases",
       "Effective for Chronic gastritis",
@@ -164,7 +148,7 @@ const products = [
   },
   {
     id: 9,
-    title:"Magilim",
+    title: "Magilim",
     benefits: [
       "Weight control",
       "It burns fat",
@@ -174,37 +158,88 @@ const products = [
   },
   {
     id: 10,
-    title:"Qinghao",
+    title: "Qinghao",
     benefits: [
-      "Treatment of all forms of malaria,especially for treatment of plasmodium falciparum malaria and multi-drug resistant malaria"
+      `Treatment of all forms of malaria,especially for treatment of plasmodium 
+      falciparum malaria and multi-drug resistant malaria`
     ]
   },
   {
     id: 11,
-    title:"Cordy Royal Jelly",
+    title: "Cordy Royal Jelly",
     benefits: [
       "For reducing blood fat",
       "Supporting healthy circulatory system",
       "Treatment of Cardiovascular diseases",
       "Improving memory"
     ]
-    
-    
-  
+  },
 
-    
-    
-      
-    
-
-
-    
-    
-
-      
-
-
-      
+  {
+    id: 12,
+    title: "Constilease",
+    benefits: [
+      "Effective for Chronic Constipation",
+      "Effective for Constipation",
+      "Effective for Bacteria Infection",
+      "Effective for Acne",
+      "Effective for Pigmentation",
+      "Effective for Halitosis",
+      "Effective for Internal hemorrhoid",
+      "Effective for Pile etc"
+    ]
+  },
+  {
+    id: 13,
+    title: "Jointeez Capsule",
+    benefits: [
+      "Effective for Osteoarthritis",
+      "Effective for Rhenmatism arthritis",
+      "Improves joint sore such as inconvenient flexion",
+      "Effective for Ankylosing spondylitis"
+    ]
+  },
+  {
+    id: 14,
+    title: "Diawell Tablet",
+    benefits: [
+      "Effective for Diabetes type 2",
+      "Effective for Chronic artrophic gastritis",
+      "Regulates blood sugar",
+      "Reduces Thirsty feeling"
+    ]
+  },
+  {
+    id: 15,
+    title: "Cardibetter Tablet",
+    benefits: [
+      "Improves heart fuction",
+      "Improves blood circulation",
+      "Effective for angina pectoris problems",
+      "Effective for choking sensation in the chest",
+      "Helpful for hypertenion"
+    ]
+  },
+  {
+    id: 16,
+    title: "Herbal Colon Cleansing Tea",
+    benefits: [
+      "It cleanses the colon and the gastro-intestinal tract",
+      "It detoxifies the body",
+      "It easese stomach and intestinal peristalsis",
+      "It boosts appetite"
+    ]
+  },
+  {
+    id: 17,
+    title: "Herbal Refresh Tea",
+    benefits: [
+      "It clears the throat ",
+      "It  makes you lively and smart",
+      "It clears internal heat and calms the body",
+      "It improves the functioning of the eyes",
+      "It is beneicail for computer users"
+    ]
   }
 ];
 export default products;
